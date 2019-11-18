cd87105b2b4f6f8be6f3faf514ba5a2e071fb533
Jean <jean.soro007@gmail.com>
2019-11-18 14:18:21 -0500

    Style and render  Services component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,17 +2,22 @@ import React from 'react';
 import HeroBackground from './HeroBackground';
 import Banner from './Banner'
 import { Link } from 'react-router-dom';
+import Services from './Services'
 
 const Home = () => {
   return (
-    <HeroBackground>
 
-      <Banner title="luxury dealership" subtitle="find your next toy here">
-        <Link to="/cars" className="btn-primary">
-          Our Cars
+    <>
+      <HeroBackground>
+
+        <Banner title="luxury dealership" subtitle="find your next toy here">
+          <Link to="/cars" className="btn-primary">
+            Our Cars
         </Link>
-      </Banner>
-    </HeroBackground>
+        </Banner>
+      </HeroBackground>
+      <Services />
+    </>
   )
 }
 
---
+++ src/Components/Services.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import Title from './Title';
+import { FaCar, FaChartLine, FaBusinessTime, FaRoad } from 'react-icons/fa';
+
+export default class Services extends Component {
+
+  state = {
+    services: [
+      {
+        icon: <FaCar />,
+        title: 'Test drive our cars!',
+        info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, illum?'
+
+      },
+      {
+        icon: <FaChartLine />,
+        title: 'Performance enhancements',
+        info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, illum?'
+
+      },
+      {
+        icon: <FaBusinessTime />,
+        title: 'Open 24/7 for our gold members',
+        info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, illum?'
+
+      },
+      {
+        icon: <FaRoad />,
+        title: 'Test drive our cars!',
+        info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, illum?'
+
+      }
+    ]
+  }
+
+
+
+  render() {
+
+    let { services } = this.state;
+
+    const servicesList = services.map((service, index) => {
+      return (
+        <article key={index} className="service">
+          <span>{service.icon}</span>
+          <h6>{service.title}</h6>
+          <p>{service.info}</p>
+        </article>
+      )
+    });
+
+    return (
+      <section className="services">
+        <Title title='Our Services' />
+        <div className="services-center">
+          {servicesList}
+        </div>
+      </section>
+    )
+  }
+}
