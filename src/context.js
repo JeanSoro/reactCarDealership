967121072d4569a83cb2397bbcab859721682243
Jean <jean.soro007@gmail.com>
2019-11-22 10:00:06 -0500

    Implement controlled input in context.js

--- src/context.js
+++ src/context.js
@@ -2,25 +2,45 @@ import React, { Component } from 'react'
 import items from './data';
 
 const CarContext = React.createContext();
+
 class CarProvider extends Component {
 
   state = {
     cars: [],
     sortedCars: [],
     featuredCars: [],
-    loading: true
+    loading: true,
+    //controlled input - cars filter component
+    type: 'all',
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false
   };
 
   // Get data
   componentDidMount() {
     let cars = this.formatData(items);
-    console.log(cars)
+
+    // return featured cars from array
     let featuredCars = cars.filter(car => car.featured === true);
+
+    //calculate default maximum price for each item in Array
+    let maxPrice = Math.max(...cars.map(car => car.price));
+    //calculate default maximum size for each item in Array
+    let maxSize = Math.max(...cars.map(car => car.size));
+
     this.setState({
       cars,
       featuredCars,
       sortedCars: cars,
-      loading: false
+      loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize
     });
   }
 
@@ -40,18 +60,29 @@ class CarProvider extends Component {
     return tempItems
   }
 
-  // getCar function
-
+  // getCar function to set up slug parameter via--react router
   getCar = (slug) => {
     let tempCars = [...this.state.cars];
     const car = tempCars.find(tempCar => tempCar.slug === slug)
     return car;
   }
 
+  //form inputs - controlled input
+  handleChange = e => {
+    const type = e.target.type;
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(type, name, value)
+  }
+
+  filterCars = () => {
+    console.log('hello')
+  }
+
 
   render() {
     return (
-      <CarContext.Provider value={{ ...this.state, getCar: this.getCar }}>
+      <CarContext.Provider value={{ ...this.state, getCar: this.getCar, handleChange: this.handleChange }}>
         {this.props.children}
       </CarContext.Provider>
     )
@@ -60,6 +91,8 @@ class CarProvider extends Component {
 
 const CarConsumer = CarContext.Consumer;
 
+
+//Higher order function that wraps component passed into in(carsContainer)
 export function withCarConsumer(Component) {
   return function ConsumerWrapper(props) {
     return <CarConsumer>
